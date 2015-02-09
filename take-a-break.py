d6e3876e14fef8f42773544d0a52121f85116924
erikssm <eriks.sm@gmail.com>
2015-02-09 20:38:20 +0200

    minor updates

--- take-a-break.py
+++ take-a-break.py
@@ -9,43 +9,34 @@ if __name__ == "__main__":
 	    sys.exit(1)
 
 	# init default variables
-	default_timeout_sec 		= 60.0 # !SECONDS	
-	default_idle_treshold		= 15.0 # minutes
-	notification_treshold_minutes 	= 60.0 # minutes
+	timeout_sec 			= 60.0 # in SECONDS	
+	idle_treshold			= 15.0 # in minutes
+	notify_minutes	 		= 60.0 # in minutes
 	mins_passed 			= 0.1
 
 	# main loop
 	while (True):
 		# sleep, count minutes slept
-		time.sleep(default_timeout_sec)
-		#mins_passed = mins_passed + ( float(default_timeout_sec) / 60.0 )
-		mins_passed = mins_passed + ( default_timeout_sec / 60.0 )
+		time.sleep(timeout_sec)		
+		mins_passed = mins_passed + ( timeout_sec / 60.0 ) # convert from sec to minutes
 		print '\nminutes passed: ' + str(mins_passed)
 
 		# if idle last N minutes then reset timer
-		p = subprocess.Popen(['xprintidle'], stdout=subprocess.PIPE)
+		p = subprocess.Popen(['xprintidle'], stdout=subprocess.PIPE) # get idle time from system
 		p.wait()
-		idle_mins = float(p.stdout.read()) / 1000 / 60  # minutes
+		idle_mins = float(p.stdout.read()) / 1000 / 60 # convert from miliseconds to minutes
 		print 'minutes idle: ' + str(round(idle_mins, 3))
-
-		if idle_mins > default_idle_treshold:
+		if idle_mins > idle_treshold:
 			mins_passed = 0.1
 
 		# show notification every hour ( or so )
-		if mins_passed > notification_treshold_minutes:
+		if mins_passed > notify_minutes:
 			n = pynotify.Notification(
 						    "Take A Break",
 						    "Remmember to take a 5 min break every hour",
 						    "dialog-information") # notification-message-im
 			n.show()
-			mins_passed = 0.1
-
-#	p = subprocess.Popen(['xprintidle'], stdout=subprocess.PIPE)
-#	p.wait()
-
-#	idle_mins = float(p.stdout.read()) / 1000 / 60  # hours
-
-
+			mins_passed = 0.1 # reset minutes counter
 
 
 	
